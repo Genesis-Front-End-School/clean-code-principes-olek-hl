1a073b18e45cc246c502c59a1587a600ec9bacf6
Anatoly Skuba <89809418+AnatolySkuba@users.noreply.github.com>
2023-04-17 16:31:36 +0300

    Update actions.ts
    Гадаю варто виправити помилку в назві на getCoursesOverview

--- src/containers/CoursesOverview/logic/actions.ts
+++ src/containers/CoursesOverview/logic/actions.ts
@@ -1,6 +1,7 @@
 import * as types from "./types";
 
 const CoursesOverviewActions = {
+  // гадаю варто виправити помилку в назві на getCoursesOverview
   getCourcesOverview: () => ({
     type: types.GET_COURSES_OVERVIEW,
   }),
